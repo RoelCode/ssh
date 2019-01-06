1d5cdef2ce74ef47850d097ac45b3d8d097155e5
Nasroel <Nasrullahharis99@gmail.com>
2019-01-07 06:41:45 +0800

    Update user-trial.sh

--- user-trial.sh
+++ user-trial.sh
@@ -8,7 +8,7 @@ Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
-echo -e "Host: $IP" 
+echo -e "============================"
 echo -e "Host: $IP" 
 echo -e "Port OpenSSH : 22, 143"
 echo -e "Port Dropbear : 442, 109, 110, 80"
@@ -19,4 +19,4 @@ echo -e "Username: $Login "
 echo -e "Password: $Pass\n"
 echo -e ""
 echo -e "Akun ini hanya aktif 1 hari"
-echo -e "============Script by.kumpul4semut================"
+echo -e "============================"
